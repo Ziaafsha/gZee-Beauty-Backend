cbaf8131b051b889c2a10879cc07328adb56fabb
Ziaafsha <mkansari008@gmail.com>
2025-04-13 17:03:52 +0530

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 2000;
 
@@ -7,7 +8,9 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api",router);
+
 
 app.listen(port, () => console.log(`SERVER STARTED ${port}`));
